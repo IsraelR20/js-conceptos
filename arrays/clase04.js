3df844f810acdb956dc4757b89c1f9149b670f6e
IsraelR20 <israelrojasvilca@gmail.com>
2023-06-12 19:19:16 -0500

    arrays

---
+++ arrays/clase04.js
@@ -0,0 +1,5 @@
+// Agregar elementos a un array en JavaScript
+const autos = ["BMW","Mercedes Benz","Volvo","Toyoya"];
+autos[autos.length] = "Cadillac";
+console.log(autos);
+console.log(autos.length);
