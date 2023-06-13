8800604611a423ca971306b2f334bf02a891dc26
IsraelR20 <israelrojasvilca@gmail.com>
2023-06-12 19:32:36 -0500

    Palabra return en JavaScript

--- funciones/clase01.js
+++ funciones/clase01.js
@@ -1,10 +1,8 @@
-//aplico el hoisting llamo antes de declarar en funciones
-miFuncion(5,10);
-
 // funcion declarativa
 function miFuncion(a,b) {
-    console.log(a+b);
+    return a+b;
 }
 
 //llamar a la función
-miFuncion(4,8);
+let resultado = miFuncion(4,8);
+console.log(resultado);
