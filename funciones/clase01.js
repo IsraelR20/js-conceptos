beb39b9e29de669196d85fcf527316e9e5f56f89
IsraelR20 <israelrojasvilca@gmail.com>
2023-06-12 20:00:00 -0500

    funcion flecha

--- funciones/clase01.js
+++ funciones/clase01.js
@@ -1,31 +1,9 @@
-// funcion declarativa
-function miFuncion(a,b) {
-    console.log(arguments.length) // 2 las propiedades de un objeto es admitido en funciones
-    return a+b;
-}
-
-//llamar a la función
-let resultado = miFuncion(4,8);
-console.log(resultado);
-
-
 // Funcion Expresion
 let x = function(a,b){return a+b};
 let resultado1 = x(1,2);
 console.log(resultado1);
 
-// Funcion self invoking (autollama) no se puede reutilizar
-(function (){
-    console.log("Ejecutando la función");
-})()
-
-(function (a,b){
-    console.log("Ejecutando la función " + (a+b));
-})(3,4)
-
-// funciones como objetos
-function funcionObjeto(a,b) {
-    return a+b;
-}
-let miFuncionTexto= funcionObjeto.toString(); // funcion que se comporta como los métodos de un objeto
-console.log(miFuncionTexto);
+// funcion flecha
+const sumarFunctionTipoFlecha = (a,b) => a+b;
+resultado1 = sumarFunctionTipoFlecha(8,2);
+console.log(resultado1);
