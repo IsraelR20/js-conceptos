df30ab4df2f2da7d09e7419adfb4b4cdaedde040
IsraelR20 <israelrojasvilca@gmail.com>
2023-06-12 19:28:12 -0500

    Funciones en JavaScript

---
+++ funciones/clase01.js
@@ -0,0 +1,10 @@
+//aplico el hoisting llamo antes de declarar en funciones
+miFuncion(5,10);
+
+// funcion declarativa
+function miFuncion(a,b) {
+    console.log(a+b);
+}
+
+//llamar a la función
+miFuncion(4,8);
