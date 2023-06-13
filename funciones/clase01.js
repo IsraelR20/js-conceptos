d0e76458c74db71990dab0a178b66b23d09456f0
IsraelR20 <israelrojasvilca@gmail.com>
2023-06-12 19:43:57 -0500

    funcion self invoking

--- funciones/clase01.js
+++ funciones/clase01.js
@@ -12,3 +12,12 @@ console.log(resultado);
 let x = function(a,b){return a+b};
 let resultado1 = x(1,2);
 console.log(resultado1);
+
+// Funcion self invoking (autollama) no se puede reutilizar
+(function (){
+    console.log("Ejecutando la función");
+})()
+
+(function (a,b){
+    console.log("Ejecutando la función " + (a+b));
+})(3,4)
