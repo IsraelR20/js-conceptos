77afe1c5445208d79546f49b847c9ce322204158
IsraelR20 <israelrojasvilca@gmail.com>
2023-06-12 19:38:32 -0500

    funciones de tipo expresion js

--- funciones/clase01.js
+++ funciones/clase01.js
@@ -6,3 +6,9 @@ function miFuncion(a,b) {
 //llamar a la función
 let resultado = miFuncion(4,8);
 console.log(resultado);
+
+
+// Funcion Expresion
+let x = function(a,b){return a+b};
+let resultado1 = x(1,2);
+console.log(resultado1);
